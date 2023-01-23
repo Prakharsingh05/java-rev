bacd1112c88cc947088968e0dbe111a34ecf3694
Prakhar singh <99808887+Prakharsingh05@users.noreply.github.com>
2023-01-23 14:43:10 +0530

    Random number genetor

---
+++ RandomNum.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+public class RandomNum {
+    
+    public static void main(String[] args){
+        Random rnd = new Random();
+
+        int RandomNumber = rnd.nextInt(20);
+
+        System.out.println("Random Number:" + RandomNumber );
+
+
+
+
+
+
+    }
+    
+}
