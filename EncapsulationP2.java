c822b1e060211028fdba3cf77ac6274dd210c74d
Prakhar singh <99808887+Prakharsingh05@users.noreply.github.com>
2023-01-24 22:01:16 +0530

    Encapsulation part 2

---
+++ EncapsulationP2.java
@@ -0,0 +1,11 @@
+ 
+public class EncapsulationP2 {
+
+    public String iamAccesibleEveryWhere;
+    private String iamAccessibleOnlyWhere;
+    protected String iamAccessibleDerivedClasses;
+
+    String iam AccessibleToAllClassInSamePackage;
+
+    
+}
