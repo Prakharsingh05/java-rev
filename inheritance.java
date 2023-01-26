68eb04d14e7c448845431b85af840c5fca26b2d3
Prakhar singh <99808887+Prakharsingh05@users.noreply.github.com>
2023-01-26 15:49:00 +0530

    day 7 (Inheritance)

---
+++ inheritance.java
@@ -0,0 +1,37 @@
+//inheritance
+
+//1.1 Memory usage Optimization
+//code reusability
+// Faster devepment
+//center codde {easy to debug / tese}
+public class Furniture { 
+
+    int width;
+    int height;
+    
+    void display(String item){
+        System.out.println(item +"Width" +width+ "Height" +height); 
+      }
+      public static void main(String[] args){
+        Chair neelkamal = new Chair();
+        neelkamal.foo();
+      }
+    }
+
+
+
+
+class Chair extends Furniture {
+    void foo() {width = 10; height=20; display("Chair");}
+         
+}
+class Table extends Furniture{
+    void foo() {width = 30; height=50; display("Table");}
+         
+
+}
+class Almira extends Furniture{
+    void foo() {width = 100; height=200; display("Almira");}
+         
+
+}
